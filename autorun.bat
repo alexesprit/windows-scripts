9da0adefd73a8d7168cfb37ff8cb03c486d2f0e9
alexesprit <alex.esprit@gmail.com>
2015-12-07 20:40:51 +0300

    Update environment variables

--- autorun.bat
+++ autorun.bat
@@ -10,11 +10,12 @@ set PROGUARD_HOME=%ANDROID_HOME%\tools\proguard
 set USERSCRIPTS=%USERPROFILE%\Documents\Scripts
 set ALIASES_FILE=%USERSCRIPTS%\aliases.ini
 
-call :add_to_path %ANDROID_HOME%\build-tools\19.0.3
+call :add_to_path %ANDROID_HOME%\build-tools\23.0.2
 call :add_to_path %ANDROID_HOME%\platform-tools
 call :add_to_path %ANDROID_HOME%\tools
 call :add_to_path %USERSCRIPTS%
 call :add_to_path %GRADLE_HOME%\bin
+call :add_to_path %APPDATA%\npm\
 
 doskey /macrofile=%ALIASES_FILE%
 
