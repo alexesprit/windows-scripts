95ae21959f1b43b548aed50bf1812b68b102cea0
alexesprit <alex.esprit@gmail.com>
2015-12-17 16:56:06 +0300

    clear_caches: clear Opera caches

--- clear_caches.bat
+++ clear_caches.bat
@@ -5,17 +5,22 @@ rem Increase free disk space!
 set IDEA_DIR=%USERPROFILE%\.IdeaIC14
 set GRADLE_DIR=%USERPROFILE%\.gradle
 
-call :clear_cache "%IDEA_DIR%\system\index"
-call :clear_cache "%IDEA_DIR%\system\caches"
+call :ClearCache "%IDEA_DIR%\system\index"
+call :ClearCache "%IDEA_DIR%\system\caches"
 
-call :clear_cache "%GRADLE_DIR%\caches"
-call :clear_cache "%GRADLE_DIR%\wrapper"
+call :ClearCache "%GRADLE_DIR%\caches"
+call :ClearCache "%GRADLE_DIR%\wrapper"
 
-call :clear_cache "%APPDATA%\npm-cache"
+call :ClearCache "%APPDATA%\npm-cache"
+
+call :ClearCache "%APPDATA%\Opera Software\Opera Stable\Application Cache"
+call :ClearCache "%APPDATA%\Opera Software\Opera Stable\GPUCache"
+call :ClearCache "%APPDATA%\Opera Software\Opera Stable\ShaderCache"
+call :ClearCache "%USERPROFILE%\AppData\Local\Opera Software\Opera Stable"
 
 exit /b
 
-:clear_cache
+:ClearCache
     if exist %1 ( 
         echo Removing %1
         rmdir /s /q %1 >nul
