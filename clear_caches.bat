3212600419053f63aed89b9f1d3705eb794099d5
alexesprit <alex.esprit@gmail.com>
2016-01-01 16:43:53 +0300

    clear_caches: add directories
    * Temp dir
    * Steam cache

--- clear_caches.bat
+++ clear_caches.bat
@@ -5,6 +5,8 @@ rem Increase free disk space!
 set IDEA_DIR=%USERPROFILE%\.IdeaIC15
 set GRADLE_DIR=%USERPROFILE%\.gradle
 
+set LOCAL_DATA=%USERPROFILE%\AppData\Local
+
 call :ClearCache "%IDEA_DIR%\system\index"
 call :ClearCache "%IDEA_DIR%\system\caches"
 
@@ -13,10 +15,14 @@ call :ClearCache "%GRADLE_DIR%\wrapper"
 
 call :ClearCache "%APPDATA%\npm-cache"
 
+call :ClearCache "%LOCAL_DATA%\Steam\htmlcache"
+
 call :ClearCache "%APPDATA%\Opera Software\Opera Stable\Application Cache"
 call :ClearCache "%APPDATA%\Opera Software\Opera Stable\GPUCache"
 call :ClearCache "%APPDATA%\Opera Software\Opera Stable\ShaderCache"
-call :ClearCache "%USERPROFILE%\AppData\Local\Opera Software\Opera Stable"
+call :ClearCache "%LOCAL_DATA%\Opera Software\Opera Stable"
+
+call :ClearCache "%LOCAL_DATA%\Temp"
 
 exit /b
 
