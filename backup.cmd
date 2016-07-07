abad76175501c87da27524f77e0fb76c9c81b662
alexesprit <alex.esprit@gmail.com>
2016-07-07 21:14:04 +0300

    Add automated backup script

---
+++ backup.cmd
@@ -0,0 +1,42 @@
+@echo off
+
+set BACKUP_DIR=%USERPROFILE%\Desktop\Backup
+
+rem Program settings
+
+call :BackupFile "%APPDATA%\ConEmu.xml"
+call :BackupFile "%APPDATA%\Imagine\Imagine.ini"
+
+call :BackupDir "%APPDATA%\AIMP"
+call :BackupDir "%APPDATA%\GHISLER"
+call :BackupDir "%APPDATA%\MPC-HC"
+call :BackupDir "%APPDATA%\Notepad++"
+call :BackupDir "%APPDATA%\Opera Software"
+call :BackupDir "%APPDATA%\Rainmeter"
+call :BackupDir "%APPDATA%\SpeedCrunch"
+call :BackupDir "%APPDATA%\uTorrent"
+
+exit /b
+
+:BackupDir
+    set source=%1
+    set target=%BACKUP_DIR%%~p1%~nx1
+
+    if not exist %target% (
+        mkdir %target%
+    )
+
+    copy %source% %target%
+    exit /b
+
+:BackupFile
+    set source=%1
+    set target=%BACKUP_DIR%%~p1
+
+    if not exist %target% (
+        mkdir %target%
+    )
+
+    copy %source% %target%
+    echo copy %source% %target%
+    exit /b
